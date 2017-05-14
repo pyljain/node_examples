811ff86b1f6d627c759004c7bb7387225eb7923e
Payal Jain <pyl.jain3@gmail.com>
2017-05-14 20:22:19 +0100

    node module example

---
+++ lessons/lesson1_modules/main.js
@@ -0,0 +1,3 @@
+const mathexamples = require('./math')
+
+console.log('The result of the math example is', mathexamples.difference(27,24));
---
+++ lessons/lesson1_modules/math.js
@@ -0,0 +1,12 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+module.exports = {
+  total : add,
+  difference : subtract
+};
